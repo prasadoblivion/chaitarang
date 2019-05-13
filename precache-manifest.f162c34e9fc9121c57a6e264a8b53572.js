0df0f5a4ee003651e891ae12d484e0d487f7245f
Prasad D <pdasanat@in.ibm.com>
2019-05-13 21:32:49 +0530

    First commit

---
+++ precache-manifest.f162c34e9fc9121c57a6e264a8b53572.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f98476d0ada367e1c66844eb6046e14",
+    "url": "/chaitarang/index.html"
+  },
+  {
+    "revision": "97442812cc5b3c4a2884",
+    "url": "/chaitarang/static/css/main.d08f8136.chunk.css"
+  },
+  {
+    "revision": "2610701bb6da8c5ae02f",
+    "url": "/chaitarang/static/js/2.692f68f2.chunk.js"
+  },
+  {
+    "revision": "97442812cc5b3c4a2884",
+    "url": "/chaitarang/static/js/main.1630f491.chunk.js"
+  },
+  {
+    "revision": "87f51b41459ab92d50f2",
+    "url": "/chaitarang/static/js/runtime~main.5d0bdf71.js"
+  },
+  {
+    "revision": "c2e1ea45ab39d0c6de5d0017abb2b28b",
+    "url": "/chaitarang/static/media/banner.c2e1ea45.png"
+  },
+  {
+    "revision": "a4cf495eaadeaeed8ceea15144446381",
+    "url": "/chaitarang/static/media/chai-logo.a4cf495e.png"
+  },
+  {
+    "revision": "fe590048509811bd1af35f5e624b3e90",
+    "url": "/chaitarang/static/media/tea_leaf.fe590048.png"
+  }
+]);
